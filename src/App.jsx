76505bd81e8c50c0332d630cfea2456857a72fab
Avi Israeli <aviv8710@gmail.com>
2024-05-29 14:41:25 +0300

    done with menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from "react";
+import menu from "./data";
+import Menu from "./Menu";
+import Title from "./Title";
+import Categories from "./Categories";
+
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <div>
+      <section className="menu">
+        <Title text={"Our Menu"} />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </div>
+  );
 };
 export default App;
